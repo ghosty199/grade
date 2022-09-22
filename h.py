cb213fa2841f58e85d2563cc9db0719cfe9aefcc
ghosty199 <88465104+ghosty199@users.noreply.github.com>
2022-09-21 18:00:09 -0600

    Add files via upload

---
+++ h.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+class historyinspace():
+    def year_2008(self):
+        print("NASA completed four space shuttle missions in 2008 to deliver modules and hardware to the International Space Station, allowing it to grow in size, volume and science capability. The flights also prepared the station to house six crew members for long-duration missions and to expand scientific exploration.")
+    def year_2011(self):
+        print("The first crewed flight of 2011 was STS-133, the final flight of the Space Shuttle Discovery, which launched from the Kennedy Space Center on 24 February. STS-133 carried Leonardo, the final American pressurised module of the ISS, for installation. Discovery returned to Earth on 9 March.")
+    
+class historyinmedical():
+    def year_2008(self):
+        print("Dec. 23, 2008&#151; -- This year marked a number of important medical advances and intriguing health news. To help narrow the large list, ABCNews.com reached out to the top medical centers and doctors in a wide range of fields.")
+    def year_2011(self):
+        print("Challenging a half century-old theory that chemotherapy works by targeting fast-dividing cells, Dana-Farber researchers report that cancer cells on the verge of self-destructing are especially vulnerable to chemotherapy.")
+class historyinsports():
+    def year_2008(self):
+        print("September 7 - In the New England Patriots' season opener, quarterback Tom Brady suffers a torn ACL that ruled him out for the season, a year after he led the Patriots to a 16-0 record (the first undefeated team since the NFL's 16-game expansion in 1978) and a Super Bowl XLII appearance. September 21 – The New England Patriots lost to the Miami Dolphins to end their NFL-record 21-game regular-season winning streak dating back to December 2006, which coincidentally, was also against the Dolphins.")
+    def year_2011(self):
+        print("Cricket February 19 – April 2 – 2011 Cricket World Cup in Sri Lanka, India, Bangladesh The tournament was won by India who defeated Sri Lanka by 6 wickets. India became the first host-nation to win the world cup with final played in the host country itself. Captain Mahendra Singh Dhoni played a captain's knock of unbeaten 91 in the final and was adjudged man of the match. Dates TBA – ICC Champions Trophy will take place. April 8 – May 28 – 2011 IPL in India. Chennai Super Kings beat Royal Challengers Bangalore by 58 runs in the final to win for the second consecutive year.")
+        
+        
+object1_space=historyinspace()
+object1_space.year_2008()
+object2_medical=historyinmedical()
+object2_medical.year_2008()
+object3_sports=historyinsports()
+object3_sports.year_2008()
